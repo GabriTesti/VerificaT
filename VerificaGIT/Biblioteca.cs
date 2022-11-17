6bde587df79672bc05390e0a2c2786d848622eeb
Vjacheslav Gabriele Testi <VJACHESLAV.TESTI@local.ittcesena.it>
2022-11-17 08:46:36 +0100

    aggiunto costruttore

---
+++ VerificaGIT/Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGIT
+{
+    internal class Biblioteca
+    {
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public string OrarioApertura { get; set; }
+        public string OrarioChiusura { get; set; }
+        public List<Libro> Libri { get; set; }
+
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            Libri = new List<Libro>();
+        }
+    }
+}
