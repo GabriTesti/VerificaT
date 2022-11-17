da1f5052023c128dbc8faf5ba104f5ae699f12b7
Vjacheslav Gabriele Testi <VJACHESLAV.TESTI@local.ittcesena.it>
2022-11-17 08:26:42 +0100

    override del ToString

---
+++ VerificaGIT/Libro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGIT
+{
+    internal class Libro
+    {
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public int Anno { get; set; }
+        public string Editore { get; set; }
+        public int NumeroPagine { get; set; }
+
+        public Libro(string autore, string titolo, int anno, string editore, int numeroPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            Anno = anno;
+            Editore = editore;
+            NumeroPagine = numeroPagine;
+        }
+        public override string ToString()
+        {
+            return Autore + " " + Titolo + " " + Anno + " " + Editore + " " + NumeroPagine;  
+        }
+    }
+}
